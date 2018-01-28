0127448ef5de34347fbc09f1151b679a69ba3fb1
Chris Carr <chris@appthem.com>
2018-01-28 14:26:15 +1100

    Removed Create & Delete functionality from Daily Rates Daily Rates initialises with one entry

--- ASPNETCoreParkingApp/Controllers/DailyRatesController.cs
+++ ASPNETCoreParkingApp/Controllers/DailyRatesController.cs
@@ -35,28 +35,6 @@ namespace ASPNETCoreParkingApp.Controllers
             return View(dailyRate);
         }
 
-        // GET: DailyRates/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: DailyRates/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(include: "ID,Charge")] DailyRate dailyRate)
-        {
-            if (ModelState.IsValid)
-            {
-                _repository.CreateNewDailyRate(dailyRate);
-                return RedirectToAction("Index");
-            }
-
-            return View(dailyRate);
-        }
-
         // GET: DailyRates/Edit/5
         public async Task<ActionResult> Edit(int? id)
         {
@@ -85,30 +63,6 @@ namespace ASPNETCoreParkingApp.Controllers
                 return RedirectToAction("Index");
             }
             return View(dailyRate);
-        }
-
-        // GET: DailyRates/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new BadRequestResult();
-            }
-            DailyRate dailyRate = _repository.GetDailyRateByID((int)id);
-            if (dailyRate == null)
-            {
-                return new NotFoundResult();
-            }
-            return View(dailyRate);
-        }
-
-        // POST: DailyRates/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
-        {
-            _repository.DeleteDailyRate((int)id);
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
--- ASPNETCoreParkingApp/Data/DbInitialiser.cs
+++ ASPNETCoreParkingApp/Data/DbInitialiser.cs
@@ -10,74 +10,79 @@ namespace ASPNETCoreParkingApp.Data
         {
             context.Database.EnsureCreated();
 
-            if (context.FlatParkingRates.Any())
+            if (!context.FlatParkingRates.Any())
             {
-                return;
+                var flatParkingRates = new FlatParkingRate[]
+                {
+                    new FlatParkingRate
+                    {
+                        Description = "Early Bird",
+                        EntryTimeStart = TimeSpan.Parse("06:00"),
+                        EntryTimeEnd = TimeSpan.Parse("09:00"),
+                        ExitTimeStart = TimeSpan.Parse("15:30"),
+                        ExitTimeEnd = TimeSpan.Parse("23:30"),
+                        EntryDays = 62,
+                        Charge = 13.00m
+                    },
+                    new FlatParkingRate
+                    {
+                        Description = "Night Rate",
+                        EntryTimeStart = TimeSpan.Parse("18:00"),
+                        EntryTimeEnd = TimeSpan.Parse("1.00:00"),
+                        ExitTimeStart = TimeSpan.Parse("18:00"),
+                        ExitTimeEnd = TimeSpan.Parse("1.06:00"),
+                        EntryDays = 62,
+                        Charge = 6.50m
+                    },
+                    new FlatParkingRate
+                    {
+                        Description = "Weekend Rate",
+                        EntryTimeStart = TimeSpan.Parse("00:00"),
+                        EntryTimeEnd = TimeSpan.Parse("2.00:00"),
+                        ExitTimeStart = TimeSpan.Parse("00:00"),
+                        ExitTimeEnd = TimeSpan.Parse("2.00:00"),
+                        EntryDays = 64,
+                        Charge = 10.00m
+                    },
+                    new FlatParkingRate
+                    {
+                        Description = "Weekend Rate",
+                        EntryTimeStart = TimeSpan.Parse("00:00"),
+                        EntryTimeEnd = TimeSpan.Parse("1.00:00"),
+                        ExitTimeStart = TimeSpan.Parse("00:00"),
+                        ExitTimeEnd = TimeSpan.Parse("1.00:00"),
+                        EntryDays = 1,
+                        Charge = 10.00m
+                    },
+                };
+                foreach (var rate in flatParkingRates)
+                {
+                    context.FlatParkingRates.Add(rate);
+                }
+                context.SaveChanges();
             }
 
-            var flatParkingRates = new FlatParkingRate[]
+            if (!context.HourlyRates.Any())
             {
-                new FlatParkingRate
+                var hourlyRates = new HourlyRate[]
                 {
-                    Description = "Early Bird",
-                    EntryTimeStart = TimeSpan.Parse("06:00"),
-                    EntryTimeEnd = TimeSpan.Parse("09:00"),
-                    ExitTimeStart = TimeSpan.Parse("15:30"),
-                    ExitTimeEnd = TimeSpan.Parse("23:30"),
-                    EntryDays = 62,
-                    Charge = 13.00m
-                },
-                new FlatParkingRate
+                    new HourlyRate {HourLimit = 1, Charge = 5.00m},
+                    new HourlyRate {HourLimit = 2, Charge = 10.00m},
+                    new HourlyRate {HourLimit = 3, Charge = 15.00m},
+                };
+                foreach (var rate in hourlyRates)
                 {
-                    Description = "Night Rate",
-                    EntryTimeStart = TimeSpan.Parse("18:00"),
-                    EntryTimeEnd = TimeSpan.Parse("1.00:00"),
-                    ExitTimeStart = TimeSpan.Parse("18:00"),
-                    ExitTimeEnd = TimeSpan.Parse("1.06:00"),
-                    EntryDays = 62,
-                    Charge = 6.50m
-                },
-                new FlatParkingRate
-                {
-                    Description = "Weekend Rate",
-                    EntryTimeStart = TimeSpan.Parse("00:00"),
-                    EntryTimeEnd = TimeSpan.Parse("2.00:00"),
-                    ExitTimeStart = TimeSpan.Parse("00:00"),
-                    ExitTimeEnd = TimeSpan.Parse("2.00:00"),
-                    EntryDays = 64,
-                    Charge = 10.00m
-                },
-                new FlatParkingRate
-                {
-                    Description = "Weekend Rate",
-                    EntryTimeStart = TimeSpan.Parse("00:00"),
-                    EntryTimeEnd = TimeSpan.Parse("1.00:00"),
-                    ExitTimeStart = TimeSpan.Parse("00:00"),
-                    ExitTimeEnd = TimeSpan.Parse("1.00:00"),
-                    EntryDays = 1,
-                    Charge = 10.00m
-                },
-            };
-            foreach (var rate in flatParkingRates)
-            {
-                context.FlatParkingRates.Add(rate);
-            }
-            context.SaveChanges();
-
-            var hourlyRates = new HourlyRate[]
-            {
-                new HourlyRate {HourLimit = 1, Charge = 5.00m},
-                new HourlyRate {HourLimit = 2, Charge = 10.00m},
-                new HourlyRate {HourLimit = 3, Charge = 15.00m},
-            };
-            foreach (var rate in hourlyRates)
-            {
-                context.HourlyRates.Add(rate);
+                    context.HourlyRates.Add(rate);
+                }
+                context.SaveChanges();
             }
 
-            var dailyRate = new DailyRate {Charge = 20.00m};
-            context.DailyRates.Add(dailyRate);
-            context.SaveChanges();
+            if (!context.DailyRates.Any())
+            {
+                var dailyRate = new DailyRate {Charge = 20.00m};
+                context.DailyRates.Add(dailyRate);
+                context.SaveChanges();
+            }
         }
     }
 }
